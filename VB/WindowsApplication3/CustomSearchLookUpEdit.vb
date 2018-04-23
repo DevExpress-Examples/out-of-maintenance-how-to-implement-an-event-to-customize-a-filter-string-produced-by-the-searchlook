2b6a0132de04b80b70dcc1d45ab02bb571d586fd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WindowsApplication3/CustomSearchLookUpEdit.vb
@@ -0,0 +1,55 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+Imports DevExpress.Skins
+Imports DevExpress.XtraEditors.Repository
+Imports DevExpress.XtraEditors.Registrator
+Imports System.Drawing
+Imports DevExpress.XtraEditors.ViewInfo
+Imports DevExpress.XtraEditors.Drawing
+Imports DevExpress.XtraEditors
+Imports System.ComponentModel
+Imports DevExpress.XtraEditors.Popup
+Imports System.Reflection
+
+Namespace DXSample
+
+	Public Class CustomSearchLookUpEdit
+		Inherits SearchLookUpEdit
+		Shared Sub New()
+			RepositoryItemCustomSearchLookUpEdit.RegisterCustomEdit()
+		End Sub
+
+		Public Sub New()
+		End Sub
+
+		Public Overrides ReadOnly Property EditorTypeName() As String
+			Get
+				Return RepositoryItemCustomSearchLookUpEdit.CustomEditName
+			End Get
+		End Property
+
+		<DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
+		Public Shadows ReadOnly Property Properties() As RepositoryItemCustomSearchLookUpEdit
+			Get
+				Return TryCast(MyBase.Properties, RepositoryItemCustomSearchLookUpEdit)
+			End Get
+		End Property
+
+		  Protected Overrides Function CreatePopupForm() As DevExpress.XtraEditors.Popup.PopupBaseForm
+			Return New CustomPopupSearchLookUpEditForm(Me)
+		  End Function
+
+		Public Custom Event UpdateDisplayFilter As UpdateDisplayFilterHandler
+			AddHandler(ByVal value As UpdateDisplayFilterHandler)
+				AddHandler Me.Properties.UpdateDisplayFilter, value
+			End AddHandler
+			RemoveHandler(ByVal value As UpdateDisplayFilterHandler)
+				RemoveHandler Me.Properties.UpdateDisplayFilter, value
+			End RemoveHandler
+			RaiseEvent(ByVal sender As Object, ByVal e As DisplayFilterEventArgs)
+			End RaiseEvent
+		End Event
+	End Class
+End Namespace
---
+++ VB/WindowsApplication3/DisplayFilterEventArgs.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+Imports DevExpress.Skins
+Imports DevExpress.XtraEditors.Repository
+Imports DevExpress.XtraEditors.Registrator
+Imports System.Drawing
+Imports DevExpress.XtraEditors.ViewInfo
+Imports DevExpress.XtraEditors.Drawing
+Imports DevExpress.XtraEditors
+Imports System.ComponentModel
+Imports DevExpress.XtraEditors.Popup
+Imports System.Reflection
+
+Namespace DXSample
+
+	Public Delegate Sub UpdateDisplayFilterHandler(ByVal sender As Object, ByVal e As DisplayFilterEventArgs)
+	Public Class DisplayFilterEventArgs
+		Inherits EventArgs
+		Private filterText_Renamed As String
+		Public Sub New(ByVal filterText As String)
+			Me.filterText_Renamed = filterText
+		End Sub
+		Public Property FilterText() As String
+			Get
+				Return filterText_Renamed
+			End Get
+			Set(ByVal value As String)
+				If filterText_Renamed <> value Then
+					filterText_Renamed = value
+				End If
+			End Set
+		End Property
+	End Class
+End Namespace
